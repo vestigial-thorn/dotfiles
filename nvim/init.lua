39918ac8be92e174196c8cb090f1ecda94637611
thorn <vestigialthorn@gmail.com>
2025-03-26 22:55:41 -0600

    adds custom color theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,16 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Function to set a colorscheme
+function SetColorscheme(name)
+  local colorscheme_path = "~/.config/nvim/colors/" .. name .. ".vim"
+  -- Check if the colorscheme file exists
+  if vim.fn.filereadable(vim.fn.expand(colorscheme_path)) == 1 then
+    vim.cmd("colorscheme " .. name)
+  else
+    print("Colorscheme not found: " .. name)
+  end
+end
+
+-- Call the function with your custom colorscheme
+SetColorscheme("darcula")
