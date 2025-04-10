9567057c8ed7bff980d8b342da043ad3a9c91d8b
thorn <vestigialthorn@gmail.com>
2025-04-10 13:04:21 -0600

    additional keymaps and colors

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,4 @@
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Default Lazyvim keymaps: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 local defaultOpts = { noremap = true, silent = true }
 
 local function keymap(mode, keys, action, opts)
@@ -9,18 +9,43 @@ local function unmap(mode, keys)
   return vim.api.nvim_del_keymap(mode, keys)
 end
 
-keymap("v", "<C-j>", ":m .+1<CR>==gv") -- move selection up; keep selected
-keymap("v", "<C-k>", ":m .-2<CR>==gv") -- move selection down; keep selected
+local function mapLeader(keys, action, desc, mode)
+  return keymap(mode or "n", ("<leader>" .. keys), action, { desc = desc, noremap = true, silent = true })
+end
+
+local function unmapLeader(keys, mode)
+  return unmap(mode or "n", ("<leader>" .. keys))
+end
+
+local function cmd(command)
+  return (":" .. command .. "<CR>")
+end
+
+mapLeader("v", "<C-j>", ":m .+1<CR>==gv") -- move selection up; keep selected
+mapLeader("v", "<C-k>", ":m .-2<CR>==gv") -- move selection down; keep selected
+keymap("n", "<leader>.", "@:", { desc = "Repeat command" })
 
 -- buffers
-unmap("n", "<leader>bd")
-unmap("n", "<leader>bD")
-keymap("n", "<leader>bk", ":b#<CR>", { desc = "Kill buffer" })
-keymap("n", "<leader>bK", ":bd<CR>", { desc = "Kill buffer and window" })
+unmapLeader("bd")
+unmapLeader("bD")
+mapLeader("bk", cmd("b#"), "Kill buffer")
+mapLeader("bK", cmd("bd"), "Kill buffer and window")
+
+--code
+keymap("n", "<C-.>", cmd("lua vim.lsp.buf.code_action()"), { desc = "Code actions" })
 
 -- files
-keymap("n", "<leader>fw", ":write<CR>", { desc = "Save file" })
+mapLeader("fw", cmd("write"), "Save file")
+mapLeader("F", cmd("Telescope find_files"), "Find files")
+
+-- git
+mapLeader("gg", cmd("vert Gitsigns diffthis HEAD"), "Diff this")
+mapLeader("gs", cmd("Gitsigns stage_hunk"), "Stage hunk")
+mapLeader("gr", cmd("Gitsigns reset_hunk"), "Reset hunk")
+
+-- ui
+Snacks.toggle.zen():map("z")
 
 --windows
-unmap("n", "<leader>wd")
-keymap("n", "<leader>wk", ":q<CR>", { desc = "Kill window" })
+unmapLeader("wd")
+mapLeader("wk", cmd("q"), "Kill window")
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,7 @@ local algae = "#698E53"
 local chalk = "#B0BDCB"
 local tomato = "#ff6347"
 local orange = "#D08B1B"
+local darkOrange = "#C8601B"
 local purple = "#B683C8"
 local slate = "#7A9EC2"
 local gray = "#737AA2"
@@ -26,18 +27,43 @@ return {
       hl["ColorColumn"] = { bg = darkGray }
       hl["CursorLine"] = { bg = darkGray }
       hl["CursorLineNr"] = { fg = tomato }
-      hl["Whitespace"] = { fg = normalFg, bg = "#752424" }
-      hl["NormalFloat"] = { bg = normalBg, fg = normalFg }
+      hl["DiagnosticUnnecessary"] = { fg = gray }
       hl["FloatBorder"] = { bg = normalBg, fg = slate }
       hl["FloatTitle"] = { bg = normalBg, fg = slate }
+      hl["NormalFloat"] = { bg = normalBg, fg = normalFg }
+      hl["Statement"] = { fg = purple }
+      hl["Whitespace"] = { fg = normalFg, bg = "#752424" }
+      hl["FlashBackdrop"] = { fg = gray }
+      hl["NeoTreeNormal"] = { bg = normalBg }
+      hl["NeoTreeNormalNC"] = { bg = normalBg }
+      hl["TelescopeNormal"] = { bg = normalBg, fg = normalFg }
+      hl["TelescopeBorder"] = { bg = normalBg, fg = slate }
+      hl["TelescopeMatching"] = { bg = normalBg }
+      hl["TelescopeNormal"] = { bg = normalBg, fg = slate }
+      hl["TelescopePromptBorder"] = { fg = orange, bg = normalBg }
+      hl["TelescopePromptNormal"] = { fg = orange }
+      hl["TelescopePromptPrefix"] = { fg = orange }
+      hl["TelescopePromptTitle"] = { fg = orange }
+      hl["TelescopeSelection"] = { bg = "#7f4c00", fg = normalFg }
+      hl["TelescopeSelectionCaret"] = { bg = normalBg, fg = orange }
+      hl["WhichKeyNormal"] = { bg = normalBg, fg = normalFg }
+
+      if vim.fn.has("nvim-0.10") == 1 then
+        hl["LineNrAbove"] = { fg = gray }
+        hl["LineNrBelow"] = { fg = gray }
+      end
+
+      ---------------
+      -- Syntax
+      ---------------
+
       hl["@attrbute"] = { fg = purple }
       hl["@constant"] = { fg = chalk }
       hl["@keyword"] = { fg = orange }
       hl["@keyword.async"] = { fg = tomato }
       hl["@keyword.import"] = { fg = orange }
-      hl["@keyword.return"] = { fg = tomato }
+      hl["@keyword.return"] = { fg = darkOrange }
       hl["@number"] = { fg = slate }
-      hl["@operator"] = { fg = chalk }
       hl["@parameter"] = { fg = normalFg }
       hl["@property"] = { fg = purple }
       hl["@punctuation.bracket"] = { fg = "#eae167" }
@@ -47,34 +73,12 @@ return {
       hl["@variable"] = { fg = chalk }
 
       --Typescript
-      hl["@variable.member.tsx"] = { fg = purple }
-      hl["@tag.tsx"] = { fg = slate }
+      hl["@keyword.coroutine.typescript"] = { fg = darkOrange }
+      hl["@lsp.type.interface.typescript"] = { fg = slate }
+      hl["@lsp.typemod.variable.defaultLibrary.typescript"] = { fg = orange }
       hl["@tag.delimiter.tsx"] = { fg = orange }
-
-      -- neo-tree
-      hl["NeoTreeNormal"] = { bg = normalBg }
-      hl["NeoTreeNormalNC"] = { bg = normalBg }
-
-      -- Telescope
-      hl["TelescopeNormal"] = { bg = normalBg, fg = normalFg }
-      hl["TelescopeBorder"] = { bg = normalBg, fg = slate }
-      hl["TelescopeSelection"] = { bg = "#7f4c00", fg = normalFg }
-      hl["TelescopeSelectionCaret"] = { bg = normalBg, fg = orange }
-      hl["TelescopePromptNormal"] = { fg = orange }
-      hl["TelescopePromptTitle"] = { fg = orange }
-      hl["TelescopePromptPrefix"] = { fg = orange }
-      hl["TelescopePromptBorder"] = { fg = orange, bg = normalBg }
-      -- hl["TelescopePreviewTitle"] = { bg = normalBg }
-      -- hl["TelescopePreviewBorder"] = { bg = normalBg }
-      -- hl["TelescopeResultsTitle"] = { bg = normalBg }
-      -- hl["TelescopeResultsBorder"] = { bg = normalBg, fg = normalFg }
-
-      hl["WhichKeyNormal"] = { bg = normalBg, fg = normalFg }
-
-      if vim.fn.has("nvim-0.10") == 1 then
-        hl.LineNrAbove = { fg = gray }
-        hl.LineNrBelow = { fg = gray }
-      end
+      hl["@tag.tsx"] = { fg = slate }
+      hl["@variable.member.tsx"] = { fg = purple }
     end,
   },
 }
